7cfdb30500ccf6de628720ef2f2895aaa1dc39b1
Manu <44656862+manuelhal@users.noreply.github.com>
2019-04-16 16:55:26 -0700

    add js typewriter effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log('ready...');
+$(document).ready(function () {
+
+    // ========================================================================= //
+    // Typed Js
+    // https://mattboldt.com/demos/typed-js/
+    // ========================================================================= //
+
+    var typed = new Typed('.typed', {
+        strings: [
+            // "Daniela.",
+            // "A Singer.",
+            // "A Songwriter.",
+            // "A Canadian.",
+            // "A Dreamer.",
+            "Singer",
+            "Songwriter",
+            "Born in Canada",
+            "Youtube sensation",
+        ],
+        typeSpeed: 100,
+        loop: true,
+    });
+
+})//end of docment ready
